3c4e8a109ef979638bfa3bb617cbddb9e174f19f
JCGrant <jamescolin.grant@gmail.com>
2017-01-06 05:16:12 +0000

    Add transform-class-properties to remove .bind(this) everywhere

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 export default class Comments extends Component {
 
-  renderComment(comment, i) {
+  renderComment = (comment, i) => {
     return (
       <div className="comment" key={i}>
         <p>
@@ -12,22 +12,22 @@ export default class Comments extends Component {
         </p>
       </div>
     );
-  }
+  };
 
-  handleSubmit(e) {
+  handleSubmit = (e) => {
     e.preventDefault();
     const postId = this.props.params.postId;
     const author = this.refs.author.value;
     const comment = this.refs.comment.value;
     this.props.addComment(postId, author, comment);
     this.refs.commentForm.reset();
-  }
+  };
 
   render() {
     return (
       <div className="comments">
         {this.props.postComments.map(this.renderComment)}
-        <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit.bind(this)}>
+        <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
           <input type="text" ref="author" placeholder="author" />
           <input type="text" ref="comment" placeholder="comment" />
           <input type="submit" hidden />
