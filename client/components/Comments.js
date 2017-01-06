ee29858cfaab5a08cd65d029cefe5fc93d358e27
JCGrant <jamescolin.grant@gmail.com>
2017-01-06 05:00:23 +0000

    Allow new comments to be added

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -20,13 +20,14 @@ export default class Comments extends Component {
     const author = this.refs.author.value;
     const comment = this.refs.comment.value;
     this.props.addComment(postId, author, comment);
+    this.refs.commentForm.reset();
   }
 
   render() {
     return (
       <div className="comments">
         {this.props.postComments.map(this.renderComment)}
-        <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
+        <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit.bind(this)}>
           <input type="text" ref="author" placeholder="author" />
           <input type="text" ref="comment" placeholder="comment" />
           <input type="submit" hidden />
--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,4 +1,25 @@
+function postComments(state=[], action) {
+  switch (action.type) {
+    case 'ADD_COMMENT':
+      console.log("Adding comment");
+      return [
+        ...state,
+        {
+          user: action.author,
+          text: action.comment
+        }
+      ];
+  }
+  return state;
+}
+
 const comments = (state=[], action) => {
+  if (typeof action.postId !== 'undefined') {
+    return {
+      ...state,
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
   return state;
 };
 
