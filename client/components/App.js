907d64f973e85f0b7b151e6ec85f81db23dacf6f
JCGrant <jamescolin.grant@gmail.com>
2017-01-06 03:40:33 +0000

    Access dispatch and state with Redux

--- client/components/App.js
+++ client/components/App.js
@@ -12,6 +12,6 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) =>
   bindActionCreators(actionCreators, dispatch);
 
-const App = connect(mapStateToProps, mapDispatchToProps);
+const App = connect(mapStateToProps, mapDispatchToProps)(Main);
 
 export default App;
